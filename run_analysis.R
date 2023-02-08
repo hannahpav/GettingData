55ee40bcf9f798411fbf6a548c86148897bc8f36
hannahpav <hannah.pavlovich@gmail.com>
2023-02-08 16:23:32 +0100

    hashed out the file read in, merged data

--- run_analysis.R
+++ run_analysis.R
@@ -2,26 +2,30 @@ library(dplyr)
 
 #check if the zip file exists
 #if not, download
-filename <- "UCI HAR Dataset.zip"
-if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileURL, filename, method = "curl")
-}
+#filename <- "UCI HAR Dataset.zip"
+#if (!file.exists(filename)){
+#  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#  download.file(fileURL, filename, method = "curl")
+#}
 
 #check if the folder exists
-#if not, download
-if(!file.exists("UCI HAR Dataset")){
-  unzip(filename)
-}
+#if not, download 
+#if(!file.exists("UCI HAR Dataset")){
+#  unzip(filename)
+#}
 
 #read in all of the files
-features <- read.table(".\UCI HAR Dataset\features.txt", col.names = c("n","features"))
-activities <- read.table(".\UCI HAR Dataset\activity_labels.txt", col.names = c("code", "activity"))
-x_train <- read.table(".\UCI HAR Dataset\train\X_train.txt", col.names = features$functions)
-y_train <- read.table(".\UCI HAR Dataset\train\Y_train.txt", col.names = "code")
-x_test <- read.table(".\UCI HAR Dataset\test\X_test.txt", col.names = features$functions)
-y_test <- read.table(".\UCI HAR Dataset\test\Y_test.txt", col.names = "code")
-subject_train <- read.table(".\UCI HAR Dataset\train\subject_train.txt", col.names = "subject")
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/Y_train.txt", col.names = "code")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/Y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 
-
-                      
+#Merge the training and test sets together
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(Subject, Y, X)
